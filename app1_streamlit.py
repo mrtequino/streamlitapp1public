b93ceacde61146b1651b56dcddfa36795a229522
dpeniafiel <www.tequino@gmail.com>
2025-05-22 22:53:27 -0500

    tercera versión

--- app1_streamlit.py
+++ app1_streamlit.py
@@ -3,5 +3,12 @@ import pyodbc
 import pandas as pd
 st.header("Hello World")
 
+st.text_input("Enter your name")
+
+def test():
+    print("test")
+
+st.button("Submit", on_click=test)
+
 SECRETO = 123
 
